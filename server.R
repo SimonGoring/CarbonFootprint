256e03d80856c8a6950929082ad1c49f5e51d111
SimonGoring <simon.j.goring@gmail.com>
2017-03-16 22:51:40 -0700

    There you go, just fixed the outline.

--- server.R
+++ server.R
@@ -45,7 +45,7 @@ shinyServer(function(input, output, session) {
       theme_bw() +
       theme(
         panel.background = element_rect(fill = "transparent"),
-        plot.background = element_rect(fill = "transparent"),
+        plot.background = element_rect(fill = "transparent", color = 'black'),
         axis.text = element_text(size = 12),
         axis.title = element_text(size = 18))
 
